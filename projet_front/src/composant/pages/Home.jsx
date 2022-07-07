890a640f2801a4a75a78c13bdc563d2512331f00
vrscode <56325294+vrscode@users.noreply.github.com>
2022-07-07 15:35:20 +0200

    changement_corentin

--- projet_front/src/composant/pages/Home.jsx
+++ projet_front/src/composant/pages/Home.jsx
@@ -4,6 +4,7 @@ import ProfilsContainer from "../ProfilsContainer";
 import { Link } from "react-router-dom";
 import HeroHeader from "../HeroHeader";
 import LocSearch from "../LocSearch";
+import Map from "../Map";
 
 
 export default function Home() {
@@ -33,10 +34,13 @@ export default function Home() {
         </section>
         <section className="sectionMap">
             <h2 className='mapTitle'>Découvrez les agriculteurs proche de chez vous</h2>
-            <div className="containerMap">
-                <div className="map">
-                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2622.919438293305!2d2.093934715675779!3d48.89787247929136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6882c0a609293%3A0x66014e53962f882f!2sCh%C3%A2teau%20de%20Saint-Germain-en-Laye!5e0!3m2!1sfr!2sfr!4v1656626397346!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
-                
+            <div className="container-map-flex">
+                <div className="containerMap">
+                    <Map/>                
+                </div>
+                <div className="container-map-txt">
+                    <h3>Où que vous soyez</h3>
+                    <p>Pour rendre accessible à tous le plus large choix de produits Bio et naturels, Packbio vous propose chaque saison le meilleur des fruits et légumes Biologiques au juste prix.</p>
                 </div>
             </div>
         </section>
