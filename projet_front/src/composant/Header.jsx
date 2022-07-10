7ac141b1a775e4024167f0693ab89a9232867bbc
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2022-07-10 19:40:57 +0200

    login

--- projet_front/src/composant/Header.jsx
+++ projet_front/src/composant/Header.jsx
@@ -25,49 +25,29 @@ export default function Header() {
 			</nav>
 		</header>
 		{modal && 
-				<form>
-				<div class="form-outline mb-4">
-				  <input type="email" id="form2Example1" class="form-control" />
-				  <label class="form-label" for="form2Example1">Email address</label>
-				</div>
-				<div class="form-outline mb-4">
-				  <input type="password" id="form2Example2" class="form-control" />
-				  <label class="form-label" for="form2Example2">Password</label>
-				</div>
-				<div class="row mb-4">
-				  <div class="col d-flex justify-content-center">
-					<div class="form-check">
-					  <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-					  <label class="form-check-label" for="form2Example31"> Remember me </label>
+				<div id="id01" class="modal">
+				<div class="modal-dialog">
+				  <div class="modal-content">
+					<header class="container"> 
+					  <a href="#" class="closebtn">×</a>
+					  <h2>Connexion</h2>
+					</header>
+					<div class="container">
+						<form action="">
+							<input type="text" id="login" name="login" placeholder="login"/>
+							<input type="text" id="password" name="login"placeholder="password"/>
+						</form>
 					</div>
-				  </div>
-			  
-				  <div class="col">
-					<a href="#!">Forgot password?</a>
+					<footer class="container">
+					<form action="">
+					<a href="">Mot de passe oublié ?</a>
+					<input type="submit" className="valide" value="Connexion"/>
+					</form>
+					</footer>
 				  </div>
 				</div>
-				<button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-				<div class="text-center">
-				  <p>Not a member? <a href="#!">Register</a></p>
-				  <p>or sign up with:</p>
-				  <button type="button" class="btn btn-link btn-floating mx-1">
-					<i class="fab fa-facebook-f"></i>
-				  </button>
-			  
-				  <button type="button" class="btn btn-link btn-floating mx-1">
-					<i class="fab fa-google"></i>
-				  </button>
-			  
-				  <button type="button" class="btn btn-link btn-floating mx-1">
-					<i class="fab fa-twitter"></i>
-				  </button>
-			  
-				  <button type="button" class="btn btn-link btn-floating mx-1">
-					<i class="fab fa-github"></i>
-				  </button>
-				</div>
-			  </form>
+			  </div> 
 				}
-		</div>
+			</div>
 	);
 }
